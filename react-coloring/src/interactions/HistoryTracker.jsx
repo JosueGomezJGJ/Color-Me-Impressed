7b6ff4464883af621e8104709ef57097a7845150
JosueGomezJGJ <99923275+JosueGomezJGJ@users.noreply.github.com>
2024-04-09 21:07:08 +0000

    redo and undo implemented

--- react-coloring/src/interactions/HistoryTracker.jsx
+++ react-coloring/src/interactions/HistoryTracker.jsx
@@ -1,36 +1,61 @@
-import { Button, Icon } from 'semantic-ui-react';
-import { useEffect } from 'react';
+import { Button, Icon } from "semantic-ui-react";
+import { useEffect } from "react";
 
-let _ = require('underscore');
-let paper = require('paper');
+let _ = require("underscore");
+let paper = require("paper");
 
-const HistoryTracker = ({paperReady, undoStack, setUndoStack, redoStack, setRedoStack})=>{
-    useEffect(() => {
-        if(paperReady && paper.project){ 
-          paper.project.clear();
-        }
-      }, [paperReady])
-
-    const undo = () => {
-        console.log("Undo pressed.")
-        // TODO
+const HistoryTracker = ({
+  paperReady,
+  undoStack,
+  setUndoStack,
+  redoStack,
+  setRedoStack,
+}) => {
+  useEffect(() => {
+    if (paperReady && paper.project) {
+      paper.project.clear();
     }
-    
-    const redo = () => {
-        console.log("Redo pressed.")
-        // TODO
+  }, [paperReady]);
+
+  const undo = () => {
+    if (undoStack.length > 0) {
+      const newUndoStack = [...undoStack];
+      const historyEvent = newUndoStack.splice(0,1)[0];
+
+      historyEvent.inverse();
+
+      const newRedoStack = [historyEvent,...redoStack];
+
+
+      setUndoStack(newUndoStack);
+      setRedoStack(newRedoStack);
     }
-    return (
-        <div>
-            <Button icon onClick={undo}>
-                <Icon name='undo' />
-            </Button>
-            <Button icon onClick={redo}>
-                <Icon name='redo' />
-            </Button>
-        </div>
-    )
-}
+  };
 
+  const redo = () => {
+    if (redoStack.length > 0) {
+      const newRedoStack = [...redoStack];
+      const historyEvent = newRedoStack.splice(0,1)[0];
 
-export {HistoryTracker};
+      historyEvent.action();
+
+      const newUndoStack = [historyEvent,...undoStack];
+
+      setUndoStack(newUndoStack);
+      setRedoStack(newRedoStack);
+    }
+  };
+
+  return (
+    <div>
+      <Button icon onClick={undo}>
+        <Icon name="undo" />
+      </Button>
+      <Button icon onClick={redo}>
+        <Icon name="redo" />
+      </Button>
+    </div>
+  );
+};
+
+export { HistoryTracker };
